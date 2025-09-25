54a84630cf76184fe48366328b61204fe9eb8d69
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2025-09-25 13:17:07 -0600

    logo contain

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -23,7 +23,7 @@ export const Logo = (props: Props) => {
       loading={loading}
       fetchPriority={priority}
       decoding="async"
-      className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
+      className={clsx('max-w-[9.375rem] w-full h-[34px] object-contain', className)}
       src={dark ? '/logo-dark.png' : '/logo.png'}
     />
   )
