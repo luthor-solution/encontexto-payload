44793e59609a153a4d2c157e840ed3ede591c183
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2025-09-26 12:20:39 -0600

    safe

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -25,6 +25,18 @@ const config = {
     'bg-success/30',
     'border-warning',
     'bg-warning/30',
+    'md:grid-cols-1',
+    'md:grid-cols-2',
+    'md:grid-cols-3',
+    'md:grid-cols-4',
+    'md:grid-cols-5',
+    'md:grid-cols-6',
+    'md:grid-cols-7',
+    'md:grid-cols-8',
+    'md:grid-cols-9',
+    'md:grid-cols-10',
+    'md:grid-cols-11',
+    'md:grid-cols-12',
   ],
   theme: {
     container: {
